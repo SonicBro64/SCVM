e560fed881eba1dad7715297eee9968b502ad0db
Artis Dembovskis <artisdembovskis@gmail.com>
2025-09-22 13:49:10 +0300

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,36 @@
+#include <SFML/Graphics.hpp>
+#include <cstdint>
+typedef uint8_t Byte;
+#include "VMkeys.hpp"
+
+#define WIDTH  640
+#define HEIGHT 480
+
+sf::Uint8 RGBAFrmBuff[WIDTH * HEIGHT * 4];
+Byte PalFrmBuff[(WIDTH / 2) * HEIGHT];
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    window.setFramerateLimit(60);
+    sf::Texture scrntex;
+    if (!scrntex.create(WIDTH, HEIGHT)) return -1;
+    sf::Sprite screen(scrntex);
+	
+	while (window.isOpen())
+		sf::Event event;
+	    while (window.pollEvent(event))
+		{
+	        if (event.type == sf::Event::Closed) window.close();
+	        int AKSize = sizeof(AllowedKeys) / sizeof(AllowedKeys[0]);
+	        for (int i=1; i<=AKsize; i++)
+				if (sf::Keyboard::isKeyPressed(AllowedKeys[i]))
+			}
+			
+		}
+		window.clear();
+		scrntex.update(RGBAFrmBuff);
+		window.draw(screen);
+		window.display();
+	}
+		
+}
